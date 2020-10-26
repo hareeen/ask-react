9d11093d5fcc32cd7a4f3ebc64d8e79e8003978f
hamerin <hamerin04@gmail.com>
2020-10-26 22:14:03 +0900

    Add question list

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import firebaseInit from './firebase';
 
-import ColorChanger from './ColorChanger'
+import ColorChanger from './ColorChanger';
 import QuestionForm from './QuestionForm';
+import QuestionList from './QuestionList';
 
 // Returns random integer choosed from range [from, to)
 function randInt(from: number, to: number): number {
@@ -15,6 +16,9 @@ export default function App() {
   const colors: String[] = ['#404759', '#467f8c', '#f2dd72', '#f2bbbb', '#382f4b', '#005daa', '#ab94fc', '#abcdef', '#c7e2cf', '#c4beef', '#ef4b3f', '#134515'];
   const [color, setColor] = useState(randInt(0, colors.length));
 
+  // 0: not update, 1: appendFront, 2: appendBack
+  let [willUpdate, setWillUpdate] = useState(2);
+
   function onColorChange() {
     let nextColor: number = randInt(0, colors.length);
     while (nextColor === color) nextColor = randInt(0, colors.length);
@@ -25,9 +29,10 @@ export default function App() {
     <div className={`c${color + 1}`}>
       <div className="hiu section full" id="first">
         <ColorChanger colorCode={colors[color]} onColorChange={onColorChange} />
-        <QuestionForm />
+        <QuestionForm onUpdate={() => setWillUpdate(1)} />
       </div>
       <div className="hiu section full" id="second">
+        <QuestionList willUpdate={willUpdate} setWillUpdate={setWillUpdate} />
       </div>
     </div>
   );
